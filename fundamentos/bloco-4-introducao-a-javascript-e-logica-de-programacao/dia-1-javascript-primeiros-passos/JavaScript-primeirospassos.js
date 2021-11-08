4f7366ee5e2ed77a760a1daa764807806d182176
Juliana Giusti <juliana.vmelo@gmail.com>
2021-11-08 11:21:35 -0300

    javascript - primeiros passos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/JavaScript-primeirospassos.js
@@ -0,0 +1,9 @@
+JavaScript-primeirospassos
+const a = 4;
+const b = 20;
+
+console.log('Soma: ' + (a+b));
+console.log('Subtração: ' + (a-b));
+console.log('Multiplicação: ' + (a*b));
+console.log('Divisão: ' + (a/b));
+console.log('Módulo: ' + (a%b));
