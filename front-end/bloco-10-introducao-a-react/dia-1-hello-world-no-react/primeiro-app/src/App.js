9372b519fa3c2578af064fc04033e71319af9440
Juliana Giusti <juliana.vmelo@gmail.com>
2022-01-21 15:59:32 -0300

    Hello World in React

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/primeiro-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Juliana from './Component';
+
+function App() {
+  return (<Juliana/>);
+
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/primeiro-app/src/Component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class Juliana extends Component {
+    render() {
+        return (<div><h1>Juliana dizendo "Hello, World!" no react</h1><p><br></br><h2>Breve descrição:</h2> Sou Engenheira e atualmente estudante de Desenvolvimento Web na Trybe!
+            Apaixonada por tecnologia, pizza e natureza!
+        </p>
+        </div>
+        )
+    }
+}
+export default Juliana
